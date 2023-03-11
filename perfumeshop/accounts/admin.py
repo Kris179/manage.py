a809b5219b76234045f805b9dafd6e317f51beb9
MedLighter <ilyaudalov2004@yandex.ru>
2023-03-11 19:51:51 +0300

    Add registration and authorization

--- perfumeshop/accounts/admin.py
+++ perfumeshop/accounts/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    pass
--- perfumeshop/accounts/views.py
+++ perfumeshop/accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib import auth
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView
 
 from .forms import UserCreationForm
@@ -8,8 +11,21 @@ from .models import User
 
 class LoginView(LoginView):
     template_name = 'accounts/auth/login.html'
+    def post(self, request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                return HttpResponseRedirect(reverse('main:index'))
 
 
 class SignUpView(CreateView):
     template_name = 'accounts/auth/signup.html'
     form_class = UserCreationForm
+
+    def post(self, request):
+        if self.form_class.is_valid():
+            return HttpResponseRedirect(reverse('accounts:login'))
+
+
